d4b2d0a4e463820b4d1557cc6ff7a9e5dc9ee5b5
Abhishek Panja <abhishek.panja554@gmail.com>
2024-09-09 06:47:35 +0530

    card flip implemented

--- lib/core/widgets/credit_card.dart
+++ lib/core/widgets/credit_card.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+
+import 'package:ez_banking/core/constants/colors.dart';
 import 'package:ez_banking/core/utilities/screen_size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CreditCard extends StatefulWidget {
   const CreditCard({super.key});
@@ -9,17 +14,204 @@ class CreditCard extends StatefulWidget {
 }
 
 class _CreditCardState extends State<CreditCard> {
+  bool _showFrontSide = true;
+  void _switchCard() {
+    setState(() {
+      _showFrontSide = !_showFrontSide;
+    });
+  }
+
+  Widget __transitionBuilder(Widget widget, Animation<double> animation) {
+    final rotateAnim = Tween(begin: pi, end: 0.0).animate(animation);
+    return AnimatedBuilder(
+      animation: rotateAnim,
+      child: widget,
+      builder: (context, widget) {
+        final isUnder = (ValueKey(_showFrontSide) != widget!.key);
+        var tilt = ((animation.value - 0.5).abs() - 0.5) * 0.003;
+        tilt *= isUnder ? -1.0 : 1.0;
+        final value =
+            isUnder ? min(rotateAnim.value, pi / 2) : rotateAnim.value;
+        return Transform(
+          transform: Matrix4.rotationY(value)..setEntry(3, 0, tilt),
+          alignment: Alignment.center,
+          child: widget,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _switchCard,
+      child: AnimatedSwitcher(
+        transitionBuilder: __transitionBuilder,
+        duration: const Duration(milliseconds: 600),
+        layoutBuilder: (widget, list) => Stack(children: [widget!, ...list]),
+        switchInCurve: Curves.easeInBack,
+        switchOutCurve: Curves.easeInBack.flipped,
+        child:
+            _showFrontSide ? const CreditCardFront() : const CreditCardBack(),
+      ),
+    );
+  }
+}
+
+class CreditCardFront extends StatelessWidget {
+  const CreditCardFront({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 20),
       width: SizeConfig.screenWidth - 40,
       height: (SizeConfig.screenWidth - 40) / 1.68,
       decoration: BoxDecoration(
         image: const DecorationImage(
-          image: AssetImage('assets/worldmap.png'),
-        ),
+            image: AssetImage('assets/worldmap.png'), fit: BoxFit.fill),
         color: Colors.white.withOpacity(0.1),
         borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SvgPicture.asset(
+                'assets/chip.svg',
+                height: 35,
+                width: 40,
+              ),
+              SvgPicture.asset(
+                'assets/wifi.svg',
+                height: 35,
+                width: 40,
+              ),
+            ],
+          ),
+          Text(
+            '1234   5678   9876   4321',
+            style: GoogleFonts.inter(
+              fontSize: 24,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Abhishek Panja',
+                style: GoogleFonts.inter(
+                  fontSize: 16,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),
+              ),
+              Column(
+                children: [
+                  SvgPicture.asset('assets/cardlogo.svg'),
+                  Text(
+                    'Master Card',
+                    style: GoogleFonts.inter(
+                      fontSize: 13,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CreditCardBack extends StatelessWidget {
+  const CreditCardBack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 20),
+      width: SizeConfig.screenWidth - 40,
+      height: (SizeConfig.screenWidth - 40) / 1.68,
+      decoration: BoxDecoration(
+        image: const DecorationImage(
+            image: AssetImage('assets/worldmap.png'), fit: BoxFit.fill),
+        color: Colors.white.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SvgPicture.asset(
+                'assets/chip.svg',
+                height: 35,
+                width: 40,
+              ),
+              SvgPicture.asset(
+                'assets/wifi.svg',
+                height: 35,
+                width: 40,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Expiry',
+                    style: GoogleFonts.inter(
+                      fontSize: 12,
+                      fontWeight: FontWeight.normal,
+                      color: textGrey,
+                    ),
+                  ),
+                  Text(
+                    '06/2027',
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'CVV',
+                    style: GoogleFonts.inter(
+                      fontSize: 12,
+                      fontWeight: FontWeight.normal,
+                      color: textGrey,
+                    ),
+                  ),
+                  Text(
+                    '320',
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
